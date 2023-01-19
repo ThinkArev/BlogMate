ac9967f6a3c6250e98a3534cdc78b128513699e8
Gagan Gupta <gagan.gupta@wakencode.com>
2023-01-19 13:33:23 +0530

    nodemon added

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 
+    // /*npx is not required when wrting nodemon start command in pacakge file
+
+
 const app = express();
-app.get('/hello',(req,res)=>{
-    res.send('Hello!');
+//middleware - checks what todo with the request
+app.use(express.json());
+
+app.post('/hello',(req,res)=>{
+    console.log(req.body);
+    res.send(req.body.name);
 })
 
 
+app.get('/hello/:name/goodbye/:otherName',(req,res)=>{
+    const { name } = req.params;
+    console.log(req.params)
+    res.send(`hello ${req.params.name}`);
+})
+
 app.listen(8000,()=>{
-    console.log("app is listening at port 3000");
+    console.log("app is listening at port 8000");
 });
