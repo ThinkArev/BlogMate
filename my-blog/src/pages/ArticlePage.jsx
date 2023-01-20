39a7ee6c805c0040e42d42162875e723954a5cb9
Gagan Gupta <gagan.gupta@wakencode.com>
2023-01-20 18:15:54 +0530

    mongo

--- my-blog/src/pages/ArticlePage.jsx
+++ my-blog/src/pages/ArticlePage.jsx
@@ -1,8 +1,15 @@
+import { useState , useEffect} from "react";
 import {useParams} from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes : 0, comments : []});
+    
+  useEffect(()=>{
+    console.log("this is useEffect");
+    setArticleInfo({upvotes : Math.ceil(Math.random() * 10), comments : []});
+  },[])
     const params = useParams();
     const   articleId  = params.articleId;
     const article = articles.find(article => article.name === articleId );
@@ -11,9 +18,11 @@ const ArticlePage = () => {
     }
     return (
       <>
-        <h1>{article.content.map((item,index) => (<p key={index}>{item}</p>)
+      <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvotes(s)</p>
+      {article.content.map((item,index) => (<p key={index}>{item}</p>)
         )} 
-        </h1>
+
       </>
         
     )
