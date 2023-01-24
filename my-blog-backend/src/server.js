5460459a1716ce9b0372db3b4480268ebfd84b34
Gagan Gupta <gagan.gupta@wakencode.com>
2023-01-24 16:29:43 +0530

    aurhtoken

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import fs from 'fs';
+import { admin } from "firebase-admin";
 // import { MongoClient } from 'mongodb'
 import { db, connectToDb } from "./db.js";
 
@@ -18,42 +20,82 @@ import { db, connectToDb } from "./db.js";
 //     comments :[],
 // }]
 
+const credentials = JSON.parse(
+fs.readFileSync('../credentials.json'));
+admin.initializeApp({
+    credential : admin.credential.cert(credentials),
+
+});
 const app = express();
 //middleware - checks what todo with the request
 app.use(express.json());
+app.use(async (req,res,next)=>{
+    const { authtoken } = req.headers;
+    if(authtoken){
+        try{
+     const user = await admin.auth().verifyIdToken(authtoken);
+    req.user = user;}
+    catch(e){
+        res.sendStatus(400);
+    }
+    }
+next();
+});
+
 app.get('/api/articles/:name', async (req,res)=>{
     const {name} = req.params
     // const client = new MongoClient('mongodb://127.0.0.1:27017');
     // await client.connect();
     // const db = client.db('react-blog-db');
-    const article = await db.collection('articles').findOne({name});
-    console.log(article)
-    res.json(article);
 
+    const article = await db.collection('articles').findOne({name});
+    if(article){
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
+        res.json(article);
+    }
+    else{
+        res.sendStatus(404);
+    }
+    console.log(article)
 })
+
 
 app.post('/hello',(req,res)=>{
     console.log(req.body);
     res.send(req.body.name);
 })
 
-app.put('/api/articles/:name/upvote', async (req,res)=>{
-    const { name } = req.params;
-    // const client = new MongoClient('mongodb://127.0.0.1:27017');
-    // await client.connect();
-    // const db = client.db('react-blog-db');
-    await db.collection('articles').updateOne({ name }, {
-        $inc : { upvotes : 1 },
-    });
-
-    const article = await db.collection('articles').findOne({ name });
-
-
-
-    if(article){
-        res.json(article)
+//put middleware
+app.use((req,res,next)=>{
+    if(req.user){
+        next();
+    }
+    else {
+        res.sendStatus(401);
+    }
 
     }
+)
+app.put('/api/articles/:name/upvote', async (req,res)=>{
+    const { name } = req.params;
+    const {uid} = req.user;
+
+    const article = await db.collection('articles').findOne({name});
+    if(article){
+        const upvoteIds = article.upvoteIds || [];
+       const canUpvote = uid && !upvoteIds.include(uid);
+       if(canUpvote){
+        await db.collection('articles').updateOne({ name }, {
+            $inc : { upvotes : 1 },
+            $push : {upvoteIds : uid}
+        });
+        // 
+       }
+       const updatedArticle = await db.collection('articles').findOne({name});
+       res.json(updatedArticle);
+    }
+  
     else 
     res.send('That article doesn\'t exist');
 });
